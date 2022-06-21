535792c9b3fc866bb8a72df37d165ca23b900667
YUN SOHEE <84841632+sh5124y@users.noreply.github.com>
2022-06-21 11:00:32 +0900

    Add files via upload

---
+++ day4/src/test/overload1/RunOverloadTest1.java
@@ -0,0 +1,30 @@
+package test.overload1;
+
+import java.util.Scanner;
+
+public class RunOverloadTest1 {
+
+   public static void main(String[] args) {
+      Scanner s= new Scanner(System.in);
+      MethodOverloadTest1 ot1 = new MethodOverloadTest1();
+      
+      System.out.print("* 정수 1 입력: ");
+      int n1 = s.nextInt();
+      System.out.print("* 정수 2 입력: ");
+      int n2 = s.nextInt();
+      
+      System.out.printf("%d * %d = %d\n",n1,n2,ot1.calc(n1,n2));
+      
+      System.out.print("* 정수 1 입력: ");
+      int n3 = s.nextInt();
+      System.out.print("* 정수 2 입력: ");
+      int n4 = s.nextInt();
+      System.out.print("* 정수 3  입력: ");
+      int n5 = s.nextInt();
+
+      System.out.printf("%d + %d + %d = %d\n",n3,n4,n5,ot1.calc(n3,n4,n5));
+      
+      s.close(); //Scanner 닫아주기
+   }
+
+}
---
+++ day4/src/test/overload2/MethodOverloadTest2.java
@@ -0,0 +1,29 @@
+package test.overload2;
+
+public class MethodOverloadTest2 {
+
+	public int calc(int n1, int n2) {
+		return n1* n2;
+	}
+	
+	public double calc(double n1, double n2) {
+		return n1 + n2;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MethodOverloadTest2 ot2 = new MethodOverloadTest2();
+		
+		int n1 = 7;
+		int n2 = 8;
+		
+		System.out.printf("%d * %d = %d\n", n1, n2, ot2.calc(n1, n2));
+		
+		double n3 = 3.5;
+		double n4 = 2.7;
+		
+		System.out.printf("%.1f + %.1f = %.2f\n", n3, n4, ot2.calc(n3, n4));
+	}
+
+}
